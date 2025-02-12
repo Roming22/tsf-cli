Roming22/tsf-cli

commit 92981a6a21a9cd3cb84f114af8bec48ca885919d

feat: deploy dh and gitops in their own namespaces
There's no good reason for the 2 services to share the same namespace.
This change helps with segregation of services. This in turn helps with
development, as it guarantee that there is no unknown link between the
2 services and one can reset a service by deleting the namespace and
redeploying the cluster.

With this change, the `rhtap` namespace becomes a location for
integration secrets.

rh-pre-commit.version: 2.3.2
rh-pre-commit.check-secrets: ENABLED

installer/charts/values.yaml.tpl
@@ -31,6 +31,9 @@ openshift:
     - rhacs-operator
     {{- end }}
 {{- end }}
+{{- if $gitops.Enabled }}
+    - {{ $gitops.Namespace }}
+{{- end }}
 {{- if $quay.Enabled }}
     - {{ $quay.Namespace }}
 {{- end }}
@@ -49,8 +52,6 @@ openshift:
 # rhtap-subscriptions
 #
 
-{{- $argoCDNamespace := .Installer.Namespace }}
-
 subscriptions:
   amqStreams:
     enabled: {{ $tpa.Enabled }}
@@ -62,7 +63,7 @@ subscriptions:
     enabled: {{ $gitops.Enabled }}
     managed: {{ and $gitops.Enabled $gitops.Properties.manageSubscription }}
     config:
-      argoCDClusterNamespace: {{ $argoCDNamespace }}
+      argoCDClusterNamespace: {{ $gitops.Namespace }}
   openshiftKeycloak:
     enabled: {{ $keycloak.Enabled }}
     managed: {{ and $keycloak.Enabled $keycloak.Properties.manageSubscription }}
@@ -103,6 +104,8 @@ minIOOperator:
 {{- $quayMinIOSecretName := "quay-minio-root-user"  }}
 
 infrastructure:
+  developerHub:
+    namespace: {{ $rhdh.Namespace }}
   kafkas:
     tpa:
       enabled: {{ $tpa.Enabled }}
@@ -142,7 +145,7 @@ infrastructure:
     patchClusterTektonConfig:
       annotations:
         meta.helm.sh/release-name: rhtap-backing-services
-        meta.helm.sh/release-namespace: {{ $argoCDNamespace }}
+        meta.helm.sh/release-namespace: {{ .Installer.Namespace }}
       labels:
         app.kubernetes.io/managed-by: Helm
 
@@ -152,7 +155,7 @@ infrastructure:
 
 {{- $keycloakRouteTLSSecretName := "keycloak-tls" }}
 {{- $keycloakRouteHost := printf "sso.%s" $ingressDomain }}
-{{- $argoCDName := "argocd" }}
+{{- $argoCDName := printf "%s-gitops" .Installer.Namespace }}
 {{- $quayMinIOHost := printf "minio-%s.%s" $quay.Namespace $ingressDomain }}
 
 backingServices:
@@ -178,11 +181,11 @@ backingServices:
       annotations:
         service.beta.openshift.io/serving-cert-secret-name: {{ $keycloakRouteTLSSecretName }}
   argoCD:
-    enabled: {{ $rhdh.Enabled }}
+    enabled: {{ $gitops.Enabled }}
     name: {{ $argoCDName }}
-    namespace: {{ $argoCDNamespace }}
-    # TODO: link this secret name with RHDH configuration.
-    secretName: rhtap-argocd-integration
+    namespace: {{ $gitops.Namespace }}
+    integrationSecret:
+      namespace: {{ .Installer.Namespace }}
     ingressDomain: {{ $ingressDomain }}
 
 #
@@ -201,14 +204,23 @@ acs: &acs
 acsTest: *acs
 
 #
+# rhtap-app-namespaces
+#
+appNamespaces:
+  argoCD:
+    name: {{ $argoCDName }}
+
+#
 # rhtap-gitops
 #
 
 argoCD:
   enabled: {{ $rhdh.Enabled }}
   name: {{ $argoCDName }}
-  namespace: {{ $argoCDNamespace }}
-  secretName: rhtap-argocd-integration
+  namespace: {{ $gitops.Namespace }}
+  integrationSecret:
+    name: rhtap-argocd-integration
+    namespace: {{ .Installer.Namespace }}
   ingressDomain: {{ $ingressDomain }}
 
 #
@@ -277,6 +289,8 @@ developerHub:
   namespace: {{ $rhdh.Namespace }}
   ingressDomain: {{ $ingressDomain }}
   catalogURL: {{ $catalogURL }}
+  integrationSecrets:
+    namespace: {{ .Installer.Namespace }}
 
 #
 # rhtap-tpa

installer/config.yaml
@@ -21,9 +21,9 @@ rhtapCLI:
         manageSubscription: true
     redHatDeveloperHub:
       enabled: &rhdhEnabled true
-      namespace: *installerNamespace
+      namespace: &rhdhNamespace rhtap-dh
       properties:
-        catalogURL: https://github.com/redhat-appstudio/tssc-sample-templates/blob/v1.4.0/all.yaml
+        catalogURL: https://github.com/redhat-appstudio/tssc-sample-templates/blob/main/all.yaml
         manageSubscription: true
     redHatAdvancedClusterSecurity:
       enabled: &rhacsEnabled true
@@ -37,7 +37,7 @@ rhtapCLI:
         manageSubscription: true
     openShiftGitOps:
       enabled: &gitopsEnabled true
-      namespace: *installerNamespace
+      namespace: &gitopsNamespace rhtap-gitops
       properties:
         manageSubscription: true
     openShiftPipelines:
@@ -66,7 +66,7 @@ rhtapCLI:
       namespace: *rhacsNamespace
       enabled: *rhacsEnabled
     - chart: charts/rhtap-gitops
-      namespace: *installerNamespace
+      namespace: *gitopsNamespace
       enabled: *gitopsEnabled
     - chart: charts/rhtap-pipelines
       namespace: *installerNamespace
@@ -84,7 +84,7 @@ rhtapCLI:
       namespace: *installerNamespace
       enabled: *pipelinesEnabled
     - chart: charts/rhtap-dh
-      namespace: *installerNamespace
+      namespace: *rhdhNamespace
       enabled: *rhdhEnabled
     - chart: charts/rhtap-acs-test
       namespace: *rhacsNamespace

commit 08c9e054ae2abf94e259f5f1369d17b070a78f66

fix: gitops SCM authentication
This was broken with 92981a6a21a9cd3cb84f114af8bec48ca885919d

rh-pre-commit.version: 2.3.2
rh-pre-commit.check-secrets: ENABLED

installer/charts/values.yaml.tpl
@@ -263,6 +263,9 @@ quay:
 integrations:
   acs:
     enabled: {{ $acs.Enabled }}
+  argoCD:
+    enabled: {{ $gitops.enabled }}
+    namespace: {{ $gitops.Namespace }}
   quay:
     enabled: {{ $quay.Enabled }}
 #   github: